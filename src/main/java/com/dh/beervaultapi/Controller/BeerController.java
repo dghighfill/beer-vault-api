62546c8c7db53ff8e6f7098d9f61d3b9305f1c49
Dale Highfill <dghighfill@gmail.com>
2019-07-20 17:16:05 -0500

    Added Get by Id

--- src/main/java/com/dh/beervaultapi/Controller/BeerController.java
+++ src/main/java/com/dh/beervaultapi/Controller/BeerController.java
@@ -3,6 +3,7 @@ package com.dh.beervaultapi.Controller;
 import com.dh.beervaultapi.dao.BeerDAO;
 import com.dh.beervaultapi.domain.Beer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,4 +22,8 @@ public class BeerController {
         return this.beerDao.getBeers();
     }
 
+    @GetMapping("/beers/{beerId}")
+    public Beer getBeerById(@PathVariable String beerId) {
+        return this.beerDao.getBeerById(beerId)
+    }
 }
